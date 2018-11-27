819354d5b3c0ab66231000ee8ed8326f16cd5884
Aparna Karve <akarve@redhat.com>
2018-11-27 13:30:45 -0800

    Create a tar.gz file with raw data
    and use Upload service to upload the file

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,8 @@
 import os
 import logging
+import requests
+import io
+import tarfile
 
 from flask import Flask, jsonify, request
 from flask.logging import default_handler
@@ -11,15 +14,43 @@ ROOT_LOGGER = logging.getLogger()
 ROOT_LOGGER.setLevel(application.logger.level)
 ROOT_LOGGER.addHandler(default_handler)
 
+# Upload Service
+UPLOAD_SERVICE = os.environ.get('UPLOAD_SERVICE')
+
 @application.route("/", methods=['POST'])
 def wake_up():
     """Endpoint for upload and publish requests."""
     input_data = request.get_json(force=True)
     data_id = input_data['id']
-    ai_service_id = input_data.get('ai_service', 'generic_ai')
     raw_data = input_data['data']
 
-    return jsonify(status='OK', message='Data published')
+    # create a tar.gz file
+    tar = tarfile.open('aiservice.tar.gz', 'w:gz')
+    data = io.BytesIO()
+    data_len = data.write(raw_data.encode())
+    info = tar.tarinfo()
+    info.name = 'aiservice_analyzed_data'
+    info.size = data_len
+
+    # add the tar.gz file to tar and close it
+    data.seek(0)
+    tar.addfile(info, data)
+    tar.close()
+
+    files = {
+        'upload': (
+        'aiservice.tar.gz', open('aiservice.tar.gz', 'rb'), 'application/vnd.redhat.aiopspublisher.aiservice+tgz')
+    }
+
+    headers = {'x-rh-insights-request-id': data_id}
+
+    # send a POST request to upload service with files and headers info
+    requests.post(f'http://{UPLOAD_SERVICE}', files=files, headers=headers)
+
+    return jsonify(
+        status='OK',
+        message='Data published via Upload service'
+    )
 
 
 if __name__ == '__main__':
