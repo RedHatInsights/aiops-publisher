5832a4cf76e04c5568b337b1dada3611ddd8bca4
Aparna Karve <aparna.karve@gmail.com>
2018-12-03 12:50:35 -0500

    Use upload service to upload data to s3 (#9)
    * Remove all s3 and kafka references
    
    * Create a tar.gz file with raw data
    
    and use Upload service to upload the file
    
    * Pipfile and Pipfile.lock adjustments
    
    * pandering to pylama
    
    * this is to ignore W0107 which is -
    
    W0107 Unnecessary pass statement [pylint]
    in tests/test_service.py
    
    * Addressed PR feedback, details below
    
    - use temp file
    - use try/except
    - create file within tar.gz with name based on AI Service id
    - remove temp file after POST call
    
    * Rename Upload service env variable to `UPLOAD_SERVICE_ENDPOINT`
    
    * Add #noqa instead of ignoring W0107
    
    * PR feedback addressed
    - simplified code block for tar write
    - use ai_service from input_data to create file inside tar
    - exception handling for POST and os.remove temp file
    - terminate app with 500 in case of errors
    
    Other than the above changes, made exception handling more generic using `except Exception as e`
    Also ensured that temp file is created with `delete=False` (an absolute must for POST to work)
    
    * add a placeholder for Retry code
    
    * Addressed PR feedback, details below
    
    - removed all # noqa's
    - broke down the long line `with tarfile.open...` into 2 smaller ones
    - removed catch-all `Exception`, now using individual and the most probable Exceptions
    - use `response.raise_for_status()` for handling non-OK responses
    
    * Addressed more feedback
    - Exception adjustments
    - exclude dashes from ai_service_id and use it as a service name

--- s3/__init__.py
+++
@@ -1,5 +0,0 @@
-"""S3 Data storage interface."""
-
-from .s3 import connect, save_data
-
-__all__ = ["connect", "save_data"]
--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,14 @@
 import os
 import logging
+import io
+import tarfile
+import tempfile
+import json
+import re
 
 from flask import Flask, jsonify, request
 from flask.logging import default_handler
-
-import s3
-import producer
+import requests
 
 application = Flask(__name__)  # noqa
 
@@ -14,14 +17,8 @@ ROOT_LOGGER = logging.getLogger()
 ROOT_LOGGER.setLevel(application.logger.level)
 ROOT_LOGGER.addHandler(default_handler)
 
-# Kafka message bus
-SERVER = os.environ.get('KAFKA_SERVER')
-TOPIC = os.environ.get('KAFKA_TOPIC')
-
-# S3 credentials
-AWS_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_BUCKET = os.environ.get('AWS_S3_BUCKET_NAME')
+# Upload Service
+UPLOAD_SERVICE_ENDPOINT = os.environ.get('UPLOAD_SERVICE_ENDPOINT')
 
 
 @application.route("/", methods=['POST'])
@@ -32,24 +29,74 @@ def wake_up():
     ai_service_id = input_data.get('ai_service', 'generic_ai')
     raw_data = input_data['data']
 
-    s3_destination = f'{AWS_BUCKET}/{data_id}/{ai_service_id}'
+    try:
+        temp_file_name = tempfile.NamedTemporaryFile(delete=False).name
+        with tarfile.open(temp_file_name, "w:gz") as tar:
+            data = io.BytesIO(json.dumps(raw_data).encode())
+            info = tarfile.TarInfo(name=f'{ai_service_id}_{data_id}.json')
+            info.size = len(data.getvalue())
+            temp_file_name = tar.name
+            tar.addfile(info, data)
 
-    application.logger.info(
-        'Saving data to location:  s3://%s', s3_destination
-    )
+    except (IOError, tarfile.TarError) as e:
+        error_msg = 'Error during TAR.GZ creation: ' + str(e)
+        ROOT_LOGGER.exception("Exception: %s", error_msg)
+        return jsonify(
+            status='Error',
+            type=str(e.__class__.__name__),
+            message=error_msg
+        ), 500
 
-    filesystem = s3.connect(AWS_KEY, AWS_SECRET)
-    s3.save_data(filesystem, s3_destination, raw_data)
-
-    message = {
-        'message': f'AI-Ops pipeline successfull for {data_id}',
-        'url': s3_destination
+    ai_service_id = re.sub(r'[^a-z]', r'', ai_service_id.lower())
+    files = {
+        'upload': (
+            temp_file_name, open(temp_file_name, 'rb'),
+            f'application/vnd.redhat.{ai_service_id}.aiservice+tgz'
+        )
     }
 
-    application.logger.info('Publishing message on topic %s', TOPIC)
-    producer.publish_message(SERVER, TOPIC, message)
+    headers = {'x-rh-insights-request-id': data_id}
 
-    return jsonify(status='OK', message='Data published')
+    # send a POST request to upload service with files and headers info
+    try:
+        response = requests.post(
+            f'http://{UPLOAD_SERVICE_ENDPOINT}',
+            files=files,
+            headers=headers
+        )
+        response.raise_for_status()
+
+    except (ConnectionError, requests.HTTPError, requests.Timeout) as e:
+        error_msg = "Error while posting data to Upload service: " + str(e)
+        ROOT_LOGGER.exception("Exception: %s", error_msg)
+
+        # TODO Implement Retry here # noqa
+        # Retry needs to examine the status_code/exact Exception type
+        # before it attempts to Retry
+        # a 415 error (Unsupported Media Type) for example,
+        # will continue to fail even in the next attempt
+        # so there is no value in pursuing a Retry for error=415
+        # A Timeout error, on the other hand, is worth Retrying
+
+        return jsonify(
+            status='Error',
+            type=str(e.__class__.__name__),
+            status_code=response.status_code,
+            message=error_msg
+        ), 500
+
+    try:
+        os.remove(temp_file_name)
+    except IOError as e:
+        # simply log the exception in this case
+        # do not return an error since this is not a critical error
+        error_msg = "Error while deleting the temporary file: " + str(e)
+        ROOT_LOGGER.exception("Exception: %s", error_msg)
+
+    return jsonify(
+        status='OK',
+        message='Data published via Upload service'
+    )
 
 
 if __name__ == '__main__':
