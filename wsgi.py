de78beb556d6e298518fed3e7588755f28e0daee
Aparna Karve <akarve@redhat.com>
2018-12-13 14:27:36 -0800

    remove swagger UI support

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@
 import connexion
 
 # Create the application instance
-application = connexion.App(__name__, specification_dir="./")   #noqa
+application = connexion.App(__name__, specification_dir="./", options={"swagger_ui": False})   #noqa
 
 # read the swagger.yml file to configure the endpoints
 application.add_api("swagger.yml")
